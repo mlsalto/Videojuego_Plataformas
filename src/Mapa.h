06c37fac47db3a8612ca27ff3abcaeb70ead8fee
aherraezduque <82113308+aherraezduque@users.noreply.github.com>
2021-05-09 10:32:40 +0200

    Add files via upload
    Clase Mapa, 
    Mi_Juego es el main que he utilizado, lo único relevante es el puntero de clase Mapa y la configuración de la escena de la función OnDraw

---
+++ src/Mapa.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include "freeglut.h"
+#include "ETSIDI.h"
+using namespace ETSIDI;
+
+class Mapa
+{
+public:
+	Mapa();
+	//~Mapa();
+
+	/*void cargarMapa();
+	void dibujarMapa();*/
+
+	void dibujarMapa();
+	void dibujarMapaptr(Mapa* ptr);
+private:
+
+	GLTexture ladrillo;//1
+	GLTexture agua;//2
+	GLTexture persona;//3
+	GLTexture aire;//0
+	GLTexture facultad;
+
+	//TO DO implementacion con asignacion dinamica de mem en funcion de las dimensiones del nivel del txt donde este
+	//diseñado el mapa
+	int nivel[10][25] {
+		{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+		{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+		{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+		{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+		{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+		{3,0,0,0,0,0,0,0,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0},
+		{1,1,1,1,1,1,1,1,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1},
+		{2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2},
+		{2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2},
+		{2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2}
+	};
+
+};
+
